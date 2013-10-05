f6c19f255d0f87383738159efcd4aba5f0599004
Bob Black <git@riblack.com>
2013-10-04 22:11:56 -0500

    Add basic test.py

---
+++ python/test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+#from migrator.utils import verbosity_loglevel
+import argparse
+import unittest
+import logging
+
+parser = argparse.ArgumentParser(description='Run tests on drive-migrator')
+# Accept testcase args when parsing, handled by uniitest.main()
+parser.add_argument('args', metavar='TEST', nargs='*',
+                    help='args?')
+parser.add_argument('--verbose', '-v', action='count',
+                    help='Increase verbosity (-v for INFO, -vv for DEBUG)')
+
+def main():
+    args = parser.parse_args()
+    logging.basicConfig()
+        #level=verbosity_loglevel(args.verbose, logging.CRITICAL))
+
+    unittest.main(module='tests')
+
+if __name__ == '__main__':
+    main()
