3fc62a081d60e0b01d7e156b00bfc821ec95e074
Bob Black <git@riblack.com>
2013-11-01 19:27:07 -0500

    Move upstream to dict config style

--- python/surfboard.py
+++ python/surfboard.py
@@ -158,37 +158,18 @@ class SignalData(object):
     down_by_column = column_getter('down',
             ('channel', 'freq', 'snr', 'power'))
 
-    UP = 'upstream'
-    up_table = table_getter(UP)
-
-    UP_CHANNEL = 'channel'
-    up_channel_row = row_getter(UP, UP_CHANNEL)
-    up_channels = field_getter(UP, UP_CHANNEL)
-
-    UP_FREQ = 'frequency'
-    up_freq_row = row_getter(UP, UP_FREQ)
-    up_freqs = field_getter(UP, UP_FREQ, ' ')
-
-    UP_SERVICE_ID = 'service id'
-    up_service_id_row = row_getter(UP, UP_SERVICE_ID)
-    up_service_ids = field_getter(UP, UP_SERVICE_ID)
-
-    UP_RATE = 'symbol rate'
-    up_rate_row = row_getter(UP, UP_RATE)
-    up_rates = field_getter(UP, UP_RATE, ' ', Decimal)
-
-    UP_POWER = 'power level'
-    up_power_row = row_getter(UP, UP_POWER)
-    up_powers = field_getter(UP, UP_POWER, ' ')
-
-    UP_STATUS = 'ranging status'
-    up_status_row = row_getter(UP, UP_STATUS)
-    up_statuses = field_getter(UP, UP_STATUS, None, str)
-
-    up_by_column = column_getter('up',
-            ('channel', 'freq', 'service_id', 'rate', 'power', 'status'))
-
     tables = {
+        'up': {
+            'header': 'upstream',
+            'rows': [
+                ('channel', 'channel'),
+                ('freq', 'frequency', ' '),
+                ('service_id', 'service id'),
+                ('rate', 'symbol rate', ' ', Decimal),
+                ('power', 'power level', ' '),
+                ('status', 'ranging status', None, str),
+            ],
+        },
         'stats': {
             'header': 'signal stats (codewords)',
             'rows': [
